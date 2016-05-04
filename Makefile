d795245ea9e3905628a493dae3641580160ad331
Timo Furrer <tuxtimo@gmail.com>
2016-05-04 09:14:25 -0700

    Configure make rule to create RabbitMQ docker container

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 publish:
 	@python3.5 setup.py sdist bdist_wheel upload
+
+docker_rabbit:
+	sudo docker run -d --hostname confluo-rabbit --name confluo-rabbit rabbitmq:3-management
--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     version=get_version(),
     license="MIT",
 
-    description="Scalable distributed microservice framework using AMQP/RabbitMQ",
+    description="Minimalist scalable distributed microservice framework using AMQP/RabbitMQ",
 
     author="Timo Furrer",
     author_email="tuxtimo@gmail.com",
